26c13ee306c3ffe4c9b25291d7656cce413aa055
André <andremessias.az@gmail.com>
2022-07-19 20:10:48 -0300

    changing the structure of the folders and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
 import {Header} from './components/Header'
-
-import {Add} from './pages/Add'
-import {WatchList} from './pages/WatchList'
-import {Watched} from './pages/Watched'
 
 import './App.css'
 import '.lib/font-awesome/css/all.min.css';
 
 function App() {
-
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <Add />
+      <Header />
+      <Outlet />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import App from './App'
+import {Add} from './pages/Add'
+import {Watched} from './pages/Watched'
+import {WatchList} from './pages/WatchList'
+
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<WatchList />} />
+          <Route path="watched" element={<Watched />} />
+          <Route path="add" element={<Add />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
