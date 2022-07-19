7e064a756e14580d679a15cb8add43ca3c4b86ad
André <andremessias.az@gmail.com>
2022-07-19 19:36:15 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Add} from './pages/Add'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Add />
+    </div>
+  )
+}
+
+export default App
