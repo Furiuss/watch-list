67658be67cb6d431324ef627ca9b7263237c8f41
André <andremessias.az@gmail.com>
2022-07-20 14:23:08 -0300

    setup global state

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -14,3 +14,15 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = props => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
 }
+
+// actions
+const addMovieToWatchlist = movie => {
+  dispatch({type: "ADD_MOVIE_TO_wATCHLIST", payload: movie})
+}
+
+return (
+  <GlobalContext.Provider 
+    value={{watchlist: state.watchlist, watched: state.watched}}>
+      {props.children}
+  </GlobalContext.Provider>
+)
