8eb8a82115d9b423a39ba140a0e2e0e43a989632
André <andremessias.az@gmail.com>
2022-07-20 11:23:38 -0300

    add result card, setup and organize css files

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './ResultCard.css'
+
 const imgURL = import.meta.env.VITE_IMG;
 
 export const ResultCard = ({movie}) => {
@@ -10,8 +12,19 @@ export const ResultCard = ({movie}) => {
           <img src={imgURL + movie.poster_path} alt={movie.title} />
         ) : (
           <div className="filler-poster"></div>
-        )
-      }
+        )}
+      </div>
+
+      <div className="info">
+        <div className="header">
+          <h3 className="title">{movie.title}</h3>
+          <h4 className="release-data">
+            {movie.release_date 
+              ? movie.release_date.substring(0, 4)
+              : "-"
+            }
+          </h4>
+        </div>
       </div>
     </div>
   )
